22aff0cfaeb5794eb3bce08d17b90a22ffac7c2d
Naimul Kabir <119662446+kabirnayeem99payvill@users.noreply.github.com>
2023-02-21 19:00:53 +0600

    feat: add svg images

--- lib/core/assets/svg_path.dart
+++ lib/core/assets/svg_path.dart
@@ -1,20 +1,13 @@
 class SvgPath {
   static const String icClock = 'assets/images/clock.svg';
-  static const String icBook = 'assets/images/book.svg';
   static const String icPaperPlane = 'assets/images/paper_plane.svg';
   static const String icHealthcare = 'assets/images/healthcare.svg';
-  static const String icSearch = 'assets/images/search_icon.svg';
   static const String icMenu = 'assets/images/menu_icon.svg';
-  static const String icHome = 'assets/images/home_icon.svg';
   static const String icNote = 'assets/images/note_icon.svg';
   static const String icSave = 'assets/images/save_icon.svg';
   static const String icBooks = 'assets/images/book_icon.svg';
   static const String icUser = 'assets/images/user_icon.svg';
-  static const String icClose = 'assets/images/close_icon.svg';
-  static const String icBookmark = 'assets/images/bookmark_icon.svg';
   static const String icAdd = 'assets/images/add_icon.svg';
-  static const String icCopy = 'assets/images/copy_icon.svg';
-  static const String icShare = 'assets/images/share_icon.svg';
   static const String icExclamation = 'assets/images/exclamation_icon.svg';
   static const String icEdit = 'assets/images/edit_icon.svg';
   static const String icDelete = 'assets/images/delete_icon.svg';
@@ -31,4 +24,70 @@ class SvgPath {
   static const String icVirticalMore = 'assets/images/vertical_more_icon.svg';
   static const String icPolygon = 'assets/images/blue_polygon.svg';
   static const String icCopyIcon = 'assets/images/copy_Icon_share_dialog.svg';
+
+  static String bgDrawer = "assets/images/svg/bg_drawer.svg";
+  static String bgHexagon = "assets/images/svg/bg_hexagon.svg";
+  static String bgHome = "assets/images/svg/bg_home.svg";
+  static String icAppearance = "assets/images/svg/ic_appearance.svg";
+  static String icAddPlan = "assets/images/svg/ic_add_plan.svg";
+  static String icBack = "assets/images/svg/ic_back.svg";
+  static String icBookmark = "assets/images/svg/ic_bookmark.svg";
+  static String icBookmarkAlt = "assets/images/svg/ic_bookmark_alt.svg";
+  static String icBookmarkFilled = "assets/images/svg/ic_bookmark_filled.svg";
+  static String icBook = "assets/images/svg/ic_book.svg";
+  static String icBookFilled = "assets/images/svg/ic_book_filled.svg";
+  static String icClose = "assets/images/svg/ic_close.svg";
+  static String icCopy = "assets/images/svg/ic_copy.svg";
+  static String icContentSettings = "assets/images/svg/ic_content_settings.svg";
+  static String icDrawerMenu = "assets/images/svg/ic_drawer_menu.svg";
+  static String icDropDown = "assets/images/svg/ic_drop_down.svg";
+  static String icExit = "assets/images/svg/ic_exit.svg";
+  static String icFontSettings = "assets/images/svg/ic_font_settings.svg";
+  static String icHizb = "assets/images/svg/ic_hizb.svg";
+  static String icHome = "assets/images/svg/ic_home.svg";
+  static String icHomeFilled = "assets/images/svg/ic_home_filled.svg";
+  static String icInfoHexagon = "assets/images/svg/ic_info_hexagon.svg";
+  static String icInfo = "assets/images/svg/ic_info.svg";
+  static String icJumpTo = "assets/images/svg/ic_jump_to.svg";
+  static String icLastRead = "assets/images/svg/ic_last_read.svg";
+  static String icList = "assets/images/svg/ic_list.svg";
+  static String icListFilled = "assets/images/svg/ic_list_filled.svg";
+  static String icLightTheme = "assets/images/svg/ic_light_theme.svg";
+  static String icMoreVert = "assets/images/svg/ic_more_vert.svg";
+  static String icNightTheme = "assets/images/svg/ic_night_theme.svg";
+  static String icPerson = "assets/images/svg/ic_person.svg";
+  static String icPersonFilled = "assets/images/svg/ic_person_filled.svg";
+  static String icSettings = "assets/images/svg/ic_settings.svg";
+  static String icSearch = "assets/images/svg/ic_search.svg";
+  static String icShare = "assets/images/svg/ic_share.svg";
+  static String icSupport = "assets/images/svg/ic_support.svg";
+  static String icTahkik = "assets/images/svg/ic_tahkik.svg";
+  static String imgLogo = "assets/images/svg/img_logo.svg";
+
+  static List<String> pathList = [
+    bgDrawer,
+    bgHexagon,
+    icAppearance,
+    icBack,
+    icBookmark,
+    icBook,
+    icContentSettings,
+    icDrawerMenu,
+    icFontSettings,
+    icHizb,
+    icHome,
+    icInfoHexagon,
+    icJumpTo,
+    icLastRead,
+    icList,
+    icLightTheme,
+    icMoreVert,
+    icNightTheme,
+    icPerson,
+    icSettings,
+    icSearch,
+    icSupport,
+    imgLogo,
+    icTahkik,
+  ];
 }
